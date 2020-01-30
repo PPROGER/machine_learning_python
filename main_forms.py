a46f40e580667a577fa8bd21bc1e8dc38b1ea5b3
Andrey Kornienko <pproger@localhost.localdomain>
2020-01-30 09:16:39 +0200

    upd main_forms

--- main_forms.py
+++ main_forms.py
@@ -13,6 +13,11 @@ class QMain(QtGui.QWidget):
         self.setGeometry(200, 200, 1600,900)
         self.setWindowTitle('Main')
         self.setWindowIcon(QtGui.QIcon('image/web.png'))
+        login = QtGui.QLabel(self)
+        login.setStyleSheet("QLabel {background:#808080; border: 1px solid #FF4500; border-radius: 10px; padding-left: 10px; margin: 5px}")
+        login.setGeometry(0,0,250,900)
+
+        
         #self.setFixedSize(400,600)
 
 
